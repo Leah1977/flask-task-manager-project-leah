54a8f16755ed95558baf684643bd03d0cf2ecf13
Leah1977 <lh020277@gmail.com>
2021-07-29 15:46:05 +0000

    Bind data from db to accordian

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
-});
-
-$(document).ready(function(){
     $('.collapsible').collapsible();
-});
+    $('.tooltipped').tooltip();
+  });
+        
